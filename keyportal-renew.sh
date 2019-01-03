b2b842901226b64d50bf26f4c07d05d8c35a08ad
Willem Hulshof <w.hulshof@magnus.nl>
2019-01-03 21:47:09 +0100

    invoice2data

---
+++ keyportal-renew.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo mv /home/voodoo/magnus-prod/.php/code/settings.php /home/voodoo/magnus-prod/
+sudo mv /home/voodoo/magnus-prod/.php/code/KeyPortal/iplog-* /home/voodoo/magnus-prod/
+sudo rm -rd /home/voodoo/magnus-prod/.php
